42dbc0c0f7b5baa0f1223fd9debf0e80a36ee433
michaelginalick <michael.ginalick@gmail.com>
2014-05-21 14:26:21 -0500

    Update my_solution.rb

--- week_8_and_9/3_Ruby/fizz_buzz/my_solution.rb
+++ week_8_and_9/3_Ruby/fizz_buzz/my_solution.rb
@@ -10,7 +10,17 @@
 # 3. Initial Solution
 
 def super_fizzbuzz(array)
-
+  array.each do |i|
+		if i % 15 == 0
+			puts "Fizzbuzz"
+		elsif i % 3 == 0
+			puts "Fizz"
+		elsif i % 5 == 0
+			puts "Buzz"
+		else
+			puts i
+		end
+	end 
 
 end
 
@@ -26,7 +36,11 @@ end
 # 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
 
 
-
+super_fizzbuzz([1,2,3])
+super_fizzbuzz([1,2,3])
+super_fizzbuzz([1,2,5])
+super_fizzbuzz([1,2,15])
+super_fizzbuzz([30, 9, 20, 1])
 
 
 
