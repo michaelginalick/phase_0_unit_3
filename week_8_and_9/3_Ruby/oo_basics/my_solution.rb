70179f70e80c30347e44ac953a84bf3eb8c6f24b
Michael <Michael.ginalick@gmail.com>
2014-05-28 02:22:46 -0500

    commit

--- week_8_and_9/3_Ruby/oo_basics/my_solution.rb
+++ week_8_and_9/3_Ruby/oo_basics/my_solution.rb
@@ -9,17 +9,55 @@
 
 # 3. Initial Solution
 
-class Student
-  attr_accessor :scores, :first_name
+class Students
+  attr_accessor :first_name, :scores
 
-  def initialize(args)   #Use named arguments!
-    #your code here
+  def initialize(first_name,scores)   #Use named arguments!
+    @first_name = first_name
+    @scores = scores
+    @average = average
+    @students = []
   end
+
+	def average
+  		  average = @scores.inject{|sum,element| sum + element} / @scores.length
+	end
+
+	def letter_grade
+		if @average >= 90
+			"A"
+		elsif @average <=89
+			"B"
+		elsif @average <=79
+			"C"
+		elsif @average <=69
+			"D"
+		else @average <=59
+			"F"
+		end
+	end
+end
+	
+ def linear_search(array,value)
+        len = array.length
+        i = 0
+    while i < len do
+        if value == Alex.first_name
+            return 0
+        end
+        i = i + 1
+    end
+        return -1
 end
 
 
+Alex = Students.new("Alex", [100,100,100,0,100])
+students=[Alex]
+4.times do Students.new("student",[rand(100),rand(100),rand(100),rand(100),rand(100)]) end
+
 # 4. Refactored Solution
 
+   
 
 
 
